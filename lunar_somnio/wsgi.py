413b7e1d190a6d7e2a0b4c1daf9cba571d7990a1
Arun Samra <3172007s@student.gla.ac.uk>
2026-02-17 17:11:40 +0000

    Initial commit

---
+++ lunar_somnio/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for lunar_somnio project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lunar_somnio.settings')
+
+application = get_wsgi_application()
