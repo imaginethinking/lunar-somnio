87a91119902f72eba95e1573623a0c0765691eac
ImagineThinking <104801799+ElotherYT@users.noreply.github.com>
2026-02-21 00:00:14 +0000

    Refactored directory structure

--- lunar_somnio_project/wsgi.py
+++ lunar_somnio_project/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for lunar_somnio project.
+WSGI config for lunar_somnio_project project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lunar_somnio.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lunar_somnio_project.settings')
 
 application = get_wsgi_application()
