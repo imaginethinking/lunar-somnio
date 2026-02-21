7bf1c3e639913be5ef391713116a6782530ad598
ImagineThinking <104801799+ElotherYT@users.noreply.github.com>
2026-02-21 01:12:05 +0000

    Implemented reaction model

--- lunar_somnio/models.py
+++ lunar_somnio/models.py
@@ -17,42 +17,59 @@ class UserProfile(models.Model):
         return self.user.username
 
 class Dream(models.Model):
-    LUCIDITY_CHOICES = [
-        (1, 'Very Low'),
-        (2, 'Low'),
-        (3, 'Medium'),
-        (4, 'High'),
-        (5, 'Very High'),
-    ]
+    class Lucidity(models.IntegerChoices):
+        VERY_LOW = 1, "Very Low"
+        LOW = 2, "Low"
+        MEDIUM = 3, "Medium"
+        HIGH = 4, "High"
+        VERY_HIGH = 5, "Very High"
 
-    COLOUR_CHOICES = []
+    class SleepQuality(models.IntegerChoices):
+        VERY_LOW = 1, "Very Low"
+        LOW = 2, "Low"
+        MEDIUM = 3, "Medium"
+        HIGH = 4, "High"
+        VERY_HIGH = 5, "Very High"
 
-    SLEEP_QUALITY_CHOICES = [
-        (1, 'Very Low'),
-        (2, 'Low'),
-        (3, 'Medium'),
-        (4, 'High'),
-        (5, 'Very High'),
-    ]
+    class Visibility(models.TextChoices):
+        PRIVATE = "private", "Private"
+        PUBLIC = "public", "Public"
 
-    VISIBILITY_CHOICES = [
-        ('private', 'Private'),
-        ('public', 'Public'),
-    ]
+    class Colour(models.TextChoices):
+        pass
 
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='dreams')
 
     title = models.CharField(max_length=255)
     text = models.TextField()
-    sleep_quality = models.IntegerField(choices=SLEEP_QUALITY_CHOICES)
+    sleep_quality = models.IntegerField(choices=SleepQuality.choices)
     dreamed_at = models.DateTimeField()
-    visibility = models.CharField(max_length=10, choices=VISIBILITY_CHOICES)
+    visibility = models.CharField(max_length=10, choices=Visibility.choices)
     image_url = models.URLField(blank=True, null=True)
     lucidity = models.IntegerField()
     nightmare = models.BooleanField()
-    colour = models.CharField(max_length=255, choices=COLOUR_CHOICES)
+    colour = models.CharField(max_length=255, choices=Colour.choices)
     recurring = models.BooleanField()
     created_at = models.DateTimeField(auto_now_add=True) # Not in ERD
     updated_at = models.DateTimeField(auto_now=True)
+
+class Reaction(models.Model):
+
+    class Emoji(models.TextChoices):
+        HEART = "❤️", "Heart"
+        LAUGH = "😂", "Laugh"
+        SURPRISED = "😮", "Surprised"
+        SAD = "😢", "Sad"
+        FIRE = "🔥", "Fire"
+
+    # Prevents one user from reacting to the same dream multiple times
+    class Meta:
+        unique_together = ("user", "dream", "emoji")
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    dream = models.ForeignKey(Dream, on_delete=models.CASCADE)
+
+    emoji = models.CharField(max_length=4, choices=Emoji.choices)
+    created_at = models.DateTimeField(auto_now_add=True)
 
 
