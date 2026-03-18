d4fd7876bd528b568cdfda17f8065945dc6f09ef
ImagineThinking <104801799+ElotherYT@users.noreply.github.com>
2026-03-18 22:37:55 +0000

    feat - add Reaction, DreamAnalysis, and WeatherSnapshot to admin page

--- lunar_somnio/admin.py
+++ lunar_somnio/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
-from .models import UserProfile, Dream, Emotion
+from .models import UserProfile, Dream, Emotion, Reaction, DreamAnalysis, WeatherSnapshot
 
 # Register your models here.
 
 admin.site.register(UserProfile)
 admin.site.register(Dream)
 admin.site.register(Emotion)
+admin.site.register(Reaction)
+admin.site.register(DreamAnalysis)
+admin.site.register(WeatherSnapshot)
